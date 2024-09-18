9bf9fddda304d97ac7d052f1c85868e2b8d58da6
t-powers <127955989+t-powers@users.noreply.github.com>
2024-09-17 21:16:38 -0400

    Iterate flockData into boilerplate

--- index.js
+++ index.js
@@ -6,3 +6,32 @@ const flockBtn = document.getElementById("flock-btn");
 flockBtn.addEventListener("click", () => {
   console.log(flockInput.value);
 });
+
+function getFeedHtml() {
+  let feedHtml = ``;
+  for (let flock of flockData) {
+    feedHtml += `<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${flock.profilePic}" class="profile-pic"/>
+        <div>
+            <p class="handle">${flock.handle}</p>
+            <p class="tweet-text">${flock.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    ${flock.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    ${flock.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${flock.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`;
+  }
+  console.log(feedHtml);
+}
+getFeedHtml();
