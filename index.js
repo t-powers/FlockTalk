df7319a702639630922074cb3a469f97075b427e
t-powers <127955989+t-powers@users.noreply.github.com>
2024-10-09 21:25:37 -0400

    Prep for reply click

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ document.addEventListener("click", function (e) {
     handleLikeClick(e.target.dataset.like);
   } else if (e.target.dataset.retweet) {
     handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply){
+    handleReplyClick(e.target.dataset.reply);
   }
 });
 
@@ -107,7 +109,7 @@ function getFeedHtml() {
             </div>   
         </div>            
     </div>
-    <div id="replies-${flock.uuid}">
+    <div class="hidden" id="replies-${flock.uuid}">
         ${repliesHtml}
     </div>
 </div>
