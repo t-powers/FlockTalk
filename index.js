2c3a4da99c5992805c58dd46263a85df1d6e0e10
t-powers <127955989+t-powers@users.noreply.github.com>
2024-10-15 21:36:54 -0400

    Update node

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { flockData } from "./data.js";
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 console.log(uuidv4());
 
-
 const flockInput = document.getElementById("flock-input");
 const flockBtn = document.getElementById("flock-btn");
 
